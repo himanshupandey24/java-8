770bd7b3d71261e0ccf1ddc3584a962f67c14d16
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-11-27 23:30:44 +0530

    added unary and binary operator

--- src/main/java/org/learnjava/functional_Interface/BinaryOperatorExample.java
+++ src/main/java/org/learnjava/functional_Interface/BinaryOperatorExample.java
@@ -1,4 +1,18 @@
 package org.learnjava.functional_Interface;
 
+import java.util.Comparator;
+import java.util.function.BinaryOperator;
+
 public class BinaryOperatorExample {
+    static Comparator<Integer> comparator = Integer::compareTo;
+
+    public static void main(String[] args) {
+        BinaryOperator<Integer> multiplication = (a,b) -> (a*b);
+        System.out.println("Multiplication is: " + multiplication.apply(10,4));
+
+        BinaryOperator<Integer> getMax = BinaryOperator.maxBy(comparator);
+        BinaryOperator<Integer> getMin = BinaryOperator.minBy(comparator);
+        System.out.println("Max is: " + getMax.apply(5, 6));
+        System.out.println("Min is: " + getMin.apply(5, 6));
+    }
 }
--- src/main/java/org/learnjava/functional_Interface/UnaryOperatorExample.java
+++ src/main/java/org/learnjava/functional_Interface/UnaryOperatorExample.java
@@ -1,4 +1,12 @@
 package org.learnjava.functional_Interface;
 
+import java.util.function.UnaryOperator;
+
 public class UnaryOperatorExample {
+
+    static UnaryOperator<String> unaryOperator = s -> s.concat("Default");
+
+    public static void main(String[] args) {
+        System.out.println(unaryOperator.apply("java17"));
+    }
 }
