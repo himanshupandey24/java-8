c2f33611cd53fccb4966965ce9089eceaae18297
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-11-27 23:39:15 +0530

    added supplier

--- src/main/java/org/learnjava/functional_Interface/SupplierExample.java
+++ src/main/java/org/learnjava/functional_Interface/SupplierExample.java
@@ -1,4 +1,26 @@
 package org.learnjava.functional_Interface;
 
+import org.learnjava.data.Student;
+import org.learnjava.data.StudentDatabase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class SupplierExample {
+
+    public static Supplier<Student> studentSupplier = () -> new Student(
+            "Adam",
+            3,
+            4,
+            "Male",
+            Arrays.asList("swimming", "basketball","volleyball"));
+
+    public static  Supplier<List<Student>> listofStudentsSupplier = () -> StudentDatabase.getStudents();
+
+    public static void main(String[] args) {
+        Student student = studentSupplier.get();
+        System.out.println("Student is : " + student);
+        System.out.println("Students are : " + listofStudentsSupplier.get());
+    }
 }
