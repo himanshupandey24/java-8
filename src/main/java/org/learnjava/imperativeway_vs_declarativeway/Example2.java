9a886471cc9d6ff953d7cfdf5ae3a4b1c624fd59
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-11-27 18:53:39 +0530

    imperative-vs-declarative

--- src/main/java/org/learnjava/imperativeway_vs_declarativeway/Example2.java
+++ src/main/java/org/learnjava/imperativeway_vs_declarativeway/Example2.java
@@ -1,4 +1,35 @@
 package org.learnjava.imperativeway_vs_declarativeway;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Example2 {
+
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(55,55,1,2,2,3,4,5,78,97,98);
+
+        //Remove the duplicates from the list.
+
+        /**
+         * Imperative Style
+         */
+        List<Integer> uniqueList = new ArrayList<>();
+        for(Integer ele : integerList){
+            if(!uniqueList.contains(ele)){
+                uniqueList.add(ele);
+            }
+        }
+
+        System.out.println("uniqueList: " +uniqueList);
+
+        /**
+         * Declarative Syle
+         */
+
+        List<Integer> uniqueList1 = integerList.stream().distinct().collect(Collectors.toList());
+
+        System.out.println("uniqueList1: " +uniqueList1);
+    }
 }
