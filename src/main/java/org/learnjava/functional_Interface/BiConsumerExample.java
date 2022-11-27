ba9bb66e2dcbb6207d966fa042993455a105ee9f
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-11-27 20:53:38 +0530

    added-bi-consumer

---
+++ src/main/java/org/learnjava/functional_Interface/BiConsumerExample.java
@@ -0,0 +1,30 @@
+package org.learnjava.functional_Interface;
+
+import org.learnjava.data.Student;
+import org.learnjava.data.StudentDatabase;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+
+public class BiConsumerExample {
+
+    public static void namesAndActivities(){
+        BiConsumer<String, List<String>> studentBiConsumer = (name, activities) -> System.out.println(name + " " + activities);
+        List<Student> studentList = StudentDatabase.getStudents();
+        studentList.forEach(student -> studentBiConsumer.accept(student.getName(), student.getActivities()));
+    }
+
+    public static void main(String[] args) {
+        BiConsumer<String, String> biConsumer = (a,b) -> System.out.println("a: " + a + ", b : " + b );
+        biConsumer.accept("java17", "java19");
+
+        BiConsumer<Integer, Integer> multiply = (a,b) -> System.out.println("Multiplication is : " + a*b);
+        BiConsumer<Integer, Integer> division = (a,b) -> System.out.println("Division is: " + a/b);
+        BiConsumer<Integer, Integer> addition = (a,b) -> System.out.println("Addition is: "+ (a+b));
+        BiConsumer<Integer, Integer> subtraction = (a,b) -> System.out.println("Subtraction is:" + (a-b));
+
+        multiply.andThen(division).andThen(addition).andThen(subtraction).accept(15,7);
+        System.out.println();
+        namesAndActivities();
+    }
+}
--- src/main/java/org/learnjava/functional_Interface/ConsumerExample.java
+++ src/main/java/org/learnjava/functional_Interface/ConsumerExample.java
@@ -8,28 +8,28 @@ import java.util.function.Consumer;
 
 public class ConsumerExample {
 
-    static Consumer<Student> c3 = s -> System.out.println(s);
-    static Consumer<Student> c4 = s -> System.out.println(s.getName().toUpperCase());
+    static Consumer<Student> c3 = student -> System.out.println(student);
+    static Consumer<Student> c4 = student -> System.out.println(student.getName().toUpperCase());
 
-    static Consumer<Student> c5 = s -> System.out.println(s.getActivities());
+    static Consumer<Student> c5 = student -> System.out.println(student.getActivities());
 
     static void printStudents(){
         List<Student> studentsList = StudentDatabase.getStudents();
         studentsList.forEach(c3);
         System.out.println();
-        studentsList.forEach(s -> System.out.println(s));
+        studentsList.forEach(student -> System.out.println(student));
     }
 
     static void printStudentNames(){
         List<Student> studentsList = StudentDatabase.getStudents();
-        studentsList.forEach(s -> System.out.println(s.getName().toUpperCase()));
+        studentsList.forEach(student -> System.out.println(student.getName().toUpperCase()));
         System.out.println();
         studentsList.forEach(c4);
     }
 
     static void printActivities(){
         List<Student> studentsList = StudentDatabase.getStudents();
-        studentsList.forEach(s -> System.out.println(s.getName() + " -> " + s.getActivities()));
+        studentsList.forEach(student -> System.out.println(student.getName() + " -> " + student.getActivities()));
         System.out.println();
         studentsList.forEach(c5);
     }
@@ -41,9 +41,9 @@ public class ConsumerExample {
 
     static void printNameAndActivitiesUsingConditions(){
         List<Student> studentList = StudentDatabase.getStudents();
-        studentList.forEach(s -> {
-            if(s.getGradeLevel() >= 3 && s.getGpa() > 3.9){
-                c4.andThen(c5).accept(s);
+        studentList.forEach(student -> {
+            if(student.getGradeLevel() >= 3 && student.getGpa() > 3.9){
+                c4.andThen(c5).accept(student);
             }
         });
     }
@@ -71,7 +71,6 @@ public class ConsumerExample {
         printNameAndActivities();
         System.out.println("\nprintNameAndActivitiesUsingConditions\n");
         printNameAndActivitiesUsingConditions();
-
     }
 
 }
