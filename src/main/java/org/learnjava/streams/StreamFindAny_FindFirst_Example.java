6fc8e8cef9f9c99073d80348ae10e86ed3e3f454
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-11-28 23:13:47 +0530

    added findany-first

--- src/main/java/org/learnjava/streams/StreamFindAny_FindFirst_Example.java
+++ src/main/java/org/learnjava/streams/StreamFindAny_FindFirst_Example.java
@@ -1,4 +1,36 @@
 package org.learnjava.streams;
 
+import org.learnjava.data.Student;
+import org.learnjava.data.StudentDatabase;
+
+import java.util.Optional;
+
 public class StreamFindAny_FindFirst_Example {
+    private static Optional<Student> findAny(){
+        return StudentDatabase.getStudents()
+                .stream()
+                .filter(student -> student.getGpa() >= 3.8)
+                .findAny();
+    }
+
+    private static Optional<Student> findFirst(){
+        return StudentDatabase.getStudents()
+                .stream()
+                .filter(student -> student.getGpa() >= 3.8)
+                .findFirst();
+    }
+
+    public static void main(String[] args) {
+        Optional<Student> findAnyStudent = findAny();
+        if(findAnyStudent.isPresent())
+            System.out.println("Student is :" + findAnyStudent.get());
+        else
+            System.out.println("No Student Found");
+
+        Optional<Student> findFirst = findFirst();
+        if(findFirst.isPresent())
+            System.out.println("Student is :" + findFirst.get());
+        else
+            System.out.println("No Student Found");
+    }
 }
