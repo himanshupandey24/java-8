b0169a985693963ed9ed30a7a843da0032998747
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-11-28 23:08:14 +0530

    added matches

--- src/main/java/org/learnjava/streams/StreamMatchExample.java
+++ src/main/java/org/learnjava/streams/StreamMatchExample.java
@@ -1,4 +1,26 @@
 package org.learnjava.streams;
 
+import org.learnjava.data.StudentDatabase;
+
 public class StreamMatchExample {
+    private static boolean allMatch(){
+        boolean result = StudentDatabase.getStudents().stream().allMatch(student -> student.getGpa() >= 3.9);
+        return result;
+    }
+
+    private static boolean anyMatch(){
+        boolean result = StudentDatabase.getStudents().stream().anyMatch(student -> student.getGpa() >= 3.9);
+        return result;
+    }
+
+    private static boolean noneMatch(){
+        boolean result = StudentDatabase.getStudents().stream().noneMatch(student -> student.getGpa() >= 3.9);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Result of allMatch : " + allMatch());
+        System.out.println("Result of anyMatch : " + anyMatch());
+        System.out.println("Result of noneMatch : " + noneMatch());
+    }
 }
