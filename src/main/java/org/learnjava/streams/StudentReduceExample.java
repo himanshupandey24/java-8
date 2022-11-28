88c86543cf2089ad464aa5e03cb676e57b6f1716
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-11-28 16:19:52 +0530

    added reduce

--- src/main/java/org/learnjava/streams/StudentReduceExample.java
+++ src/main/java/org/learnjava/streams/StudentReduceExample.java
@@ -3,8 +3,10 @@ package org.learnjava.streams;
 import org.learnjava.data.Student;
 import org.learnjava.data.StudentDatabase;
 
+import javax.swing.text.html.Option;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class StudentReduceExample {
 
@@ -12,6 +14,12 @@ public class StudentReduceExample {
         return integerList
                 .stream()
                 .reduce(1,(a,b) -> a*b);
+    }
+
+    private static Optional<Integer> performMultiplicationWithoutIdentity(List<Integer> integerList){
+        return integerList
+                .stream()
+                .reduce((a,b) -> a*b);
     }
 
     private static String combineStudents(){
@@ -22,9 +30,27 @@ public class StudentReduceExample {
                 .reduce("", String::concat); // or .reduce("", (s1, s2) -> s1.concat(s2) );
     }
 
+    private static Optional<Student> getHighestGradeStudent(){
+        Optional<Student> studentOptional = StudentDatabase.getStudents()
+                .stream()
+                .reduce((s1,s2) -> s1.getGpa() > s2.getGpa() ? s1 : s2);
+        return studentOptional;
+    }
+
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(1,3,5,7,9);
         System.out.println(performMultiplication(integerList));
+
+        Optional<Integer> result = performMultiplicationWithoutIdentity(integerList);
+        if(result.isPresent()){
+            System.out.println("Result is : " + result.get());
+        }
+        else{
+            System.out.println("Result is : " + 0);
+        }
+
         System.out.println("\n" + combineStudents());
+        System.out.println("\n" + getHighestGradeStudent().get());
+
     }
 }
