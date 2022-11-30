1f16e42fad64638e8eafdb97f57cc999fd63ec87
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-11-30 16:51:11 +0530

    collectors mapping

--- src/main/java/org/learnjava/streams_terminal/StreamaMappingExample.java
+++ src/main/java/org/learnjava/streams_terminal/StreamaMappingExample.java
@@ -1,4 +1,24 @@
 package org.learnjava.streams_terminal;
 
+import org.learnjava.data.Student;
+import org.learnjava.data.StudentDatabase;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class StreamaMappingExample {
+    public static void main(String[] args) {
+        Set<String> namesSet = StudentDatabase.getStudents()
+                .stream()
+                .collect(Collectors.mapping(Student::getName, Collectors.toSet()));
+
+        System.out.println("namesSet : " + namesSet);
+
+        List<String> namesList = StudentDatabase.getStudents()
+                .stream()
+                .collect(Collectors.mapping(Student::getName, Collectors.toList()));
+
+        System.out.println("namesList : " + namesList);
+    }
 }
