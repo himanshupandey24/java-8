9e11d6c4e9f75319eb3ca17cf6f8ff438568ebdb
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-11-30 15:50:03 +0530

    added boxing and unboxing

---
+++ src/main/java/org/learnjava/numericstreams/NumericStreamBoxingUnboxingExample.java
@@ -0,0 +1,33 @@
+package org.learnjava.numericstreams;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class NumericStreamBoxingUnboxingExample {
+
+    private static List<Integer> boxing(){
+        return IntStream.rangeClosed(1,50)
+                //Stream<Integer>
+                .boxed()
+                //all the elements will be passed one by one to get to the result as the collection.
+                .collect(Collectors.toList());
+
+    }
+
+    private static int unBoxing(List<Integer> integerList){
+        int sum = integerList.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        Optional<Integer> sum1 = integerList.stream()
+                .reduce((x, y) -> x+y);
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Converting PrimitiveStream to List : "+ boxing());
+        List<Integer> integerList = boxing();
+        System.out.println("Sum using Unboxing : "  + unBoxing(integerList));
+    }
+}
