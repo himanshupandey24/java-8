389bb9df08f3ff32e92a88f821359613d62e5d37
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-12-01 18:11:28 +0530

    added Optional1

--- src/main/java/org/learnjava/optional/OptionalOfEmptyNullableExample.java
+++ src/main/java/org/learnjava/optional/OptionalOfEmptyNullableExample.java
@@ -1,4 +1,23 @@
 package org.learnjava.optional;
 
+import java.util.Optional;
+
 public class OptionalOfEmptyNullableExample {
+
+    private static Optional<String> ofNullable(){
+        return Optional.ofNullable(null);
+    }
+
+    private static Optional<String> of(){
+        return Optional.of("Hello");
+    }
+
+    private static Optional<String> empty(){
+        return Optional.empty();
+    }
+    public static void main(String[] args) {
+         System.out.println(ofNullable().get()); //NoSuchElementException: No value present
+         System.out.println(of());
+         System.out.println(empty().get()); //NoSuchElementException: No value present
+    }
 }
