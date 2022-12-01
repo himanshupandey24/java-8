859ecb7bfc518c7466115fdd318badc33e34ec6a
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-12-01 17:22:45 +0530

    added partitionBy

---
+++ src/main/java/org/learnjava/parallelstream/ParallelStreamsBoxedExample.java
@@ -0,0 +1,7 @@
+package org.learnjava.parallelstream;
+
+public class ParallelStreamsBoxedExample {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/learnjava/parallelstream/SumClient.java
@@ -0,0 +1,2 @@
+package org.learnjava.parallelstream;public class SumClient {
+}
