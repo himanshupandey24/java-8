6bbd700206cbaec3e1f72a4695bf2dd929c78aca
Himanshu Pandey <himanshupandey2411@gmail.com>
2022-12-13 16:27:42 +0530

    added arraylistenhancements

--- src/main/java/org/learnjava/collections/ArrayListEnhancements.java
+++ src/main/java/org/learnjava/collections/ArrayListEnhancements.java
@@ -1,7 +1,30 @@
 package org.learnjava.collections;
 
-public class ArrayListEnhancements {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class ArrayListEnhancements {
+
+    private static List<String> names = Arrays.asList("Dave", "Jessica", "Alex", "Dan");
+    private static void listForEach(){
+        for(String name : names)
+            System.out.print(name + " ");
+
+        System.out.println();
+
+        names.forEach(System.out::println);
+
+
+        List<String> strings = Arrays.asList("one", "two", "three", "four");
+        AtomicInteger count = new AtomicInteger();
+
+        strings.forEach(name ->{
+            System.out.println("name : " + name);
+            System.out.println(count.getAndIncrement());
+        });
+    }
+    public static void main(String[] args) {
+        listForEach();
     }
 }
